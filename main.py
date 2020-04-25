03e8de130a4d14fda36c492e64b975985076fcfc
kaij <daniel.jameson@aggiemail.usu.edu>
2020-04-24 20:12:23 -0600

    downloaded csv and fixed the api key problems

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot as plt
-from api_key.txt import api_key
-dataset = read_csv('https://www.quandl.com/api/v3/datasets/EOD/WMT.csv?api_key=' + api_key)
-print(dataset)
+from api_key import api_key
+
