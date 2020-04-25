60dc3edef2cd7043aded9f4e6f01d3bc1377b56a
kaij <daniel.jameson@aggiemail.usu.edu>
2020-04-25 17:17:40 -0600

    had 2 of the class names pushed together accidently

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot as plt
 from api_key import api_key
-names = ['Date','Open','High','Low','Close','Volume','Dividend','Split,Adj_Open','Adj_High','Adj_Low','Adj_Close','Adj_Volume']
+names = ['Date','Open','High','Low','Close','Volume','Dividend','Split','Adj_Open','Adj_High','Adj_Low','Adj_Close','Adj_Volume']
 #dataset = read_csv('https://www.quandl.com/api/v3/datasets/EOD/WMT.csv?api_key=' + api_key)
 #print(dataset)
 
