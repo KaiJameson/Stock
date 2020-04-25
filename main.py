02b9fc8018bdcff1203d813fcff98231db6c9148
kaij <daniel.jameson@aggiemail.usu.edu>
2020-04-25 17:57:27 -0600

    read in the dataset

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from api_key import api_key
 names = ['Date','Open','High','Low','Close','Volume','Dividend','Split','Adj_Open','Adj_High','Adj_Low','Adj_Close','Adj_Volume']
 #dataset = read_csv('https://www.quandl.com/api/v3/datasets/EOD/WMT.csv?api_key=' + api_key)
 #print(dataset)
-
+dataset = read_csv('EOD-WMT.csv', names=names)
+print(dataset)
