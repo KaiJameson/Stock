c0ccfad2018c6246b37bae317a38c236a68a374f
kaij <daniel.jameson@aggiemail.usu.edu>
2020-05-22 14:45:07 -0400

    have an environment file to store variables

---
+++ scripts/environment.py
@@ -0,0 +1,19 @@
+
+
+
+test_var = 'open'
+reports_directory = 'reports'
+error_file = 'error_file.txt'
+tuning_status_file = 'status.txt'
+config_directory = 'config'
+stock_decisions_directory = 'information'
+graph_directory = 'plots'
+
+
+
+
+
+
+
+
+
