ab1c2040bfb4b94777186b4aa73cbcb1001c5379
kaij <daniel.jameson@aggiemail.usu.edu>
2020-05-22 15:11:20 -0400

    added error_file to the gitignore

--- scripts/environment.py
+++ scripts/environment.py
@@ -2,12 +2,12 @@
 
 money = 10000
 test_var = 'open'
-reports_directory = 'reports'
-error_file = 'error_file.txt'
-tuning_status_file = 'status.txt'
-config_directory = 'config'
-stock_decisions_directory = 'information'
-graph_directory = 'plots'
+reports_directory = '../reports'
+error_file = '../error_file.txt'
+tuning_status_file = '../status.txt'
+config_directory = '../config'
+stock_decisions_directory = '../information'
+graph_directory = '../plots'
 random_seed = 314
 
 
