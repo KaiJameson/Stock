b13e07841837d6df5755642fd2e436724b79f711
Spencer Howlett <spencer.v.howlett@gmail.com>
2021-01-29 14:54:23 -0700

    added some new indexs to use for features

--- scripts/batch_run.py
+++ scripts/batch_run.py
@@ -8,19 +8,19 @@ import sys
 
 check_directories()
 
-# time_s = time.time()
-# print("test 1")
-# the_real_test(test_year, test_month, test_day, test_days, 
-# ["open", "low", "high", "close", "mid", "volume"],
-# n_steps=300, lookup_step=1, test_size=.2, n_layers=2, cell=LSTM, units=256, dropout=.4, 
-# bidirectional=False, loss="huber_loss", optimizer="adam", batch_size=256, 
-# epochs=800, patience=200, saveload=True, limit=4000)
-# print("test 1 took " + str(time.time() - time_s))
+time_s = time.time()
+print("test 1")
+the_real_test(test_year, test_month, test_day, test_days, 
+["open", "low", "high", "close", "mid", "volume", "midpoint"],
+n_steps=300, lookup_step=1, test_size=.2, n_layers=2, cell=LSTM, units=256, dropout=.4, 
+bidirectional=False, loss="huber_loss", optimizer="adam", batch_size=256, 
+epochs=800, patience=200, saveload=True, limit=4000)
+print("test 1 took " + str(time.time() - time_s))
 
 time_s = time.time()
 print("test 2")
 the_real_test(test_year, test_month, test_day, test_days, 
-["open", "low", "high", "close", "mid", "volume", "time_series_forecast"],
+["open", "low", "high", "close", "mid", "volume", "avg_price"],
 n_steps=300, lookup_step=1, test_size=.2, n_layers=2, cell=LSTM, units=256, dropout=.4, 
 bidirectional=False, loss="huber_loss", optimizer="adam", batch_size=256, 
 epochs=800, patience=200, saveload=True, limit=4000)
@@ -29,7 +29,7 @@ print("test 2 took " + str(time.time() - time_s))
 time_s = time.time()
 print("test 3")
 the_real_test(test_year, test_month, test_day, test_days, 
-["open", "low", "high", "close", "mid", "volume", "minus_directional_move"],
+["open", "low", "high", "close", "mid", "volume", "weighted_close_price"],
 n_steps=300, lookup_step=1, test_size=.2, n_layers=2, cell=LSTM, units=256, dropout=.4, 
 bidirectional=False, loss="huber_loss", optimizer="adam", batch_size=256, 
 epochs=800, patience=200, saveload=True, limit=4000)
@@ -38,20 +38,20 @@ print("test 3 took " + str(time.time() - time_s))
 time_s = time.time()
 print("test 4")
 the_real_test(test_year, test_month, test_day, test_days, 
-["open", "low", "high", "close", "mid", "volume", "lin_regres"],
+["open", "low", "high", "close", "mid", "volume", "midprice"],
 n_steps=300, lookup_step=1, test_size=.2, n_layers=2, cell=LSTM, units=256, dropout=.4, 
 bidirectional=False, loss="huber_loss", optimizer="adam", batch_size=256, 
 epochs=800, patience=200, saveload=True, limit=4000)
 print("test 4 took " + str(time.time() - time_s))
 
-time_s = time.time()
-print("test 5")
-the_real_test(test_year, test_month, test_day, test_days, 
-["open", "low", "high", "close", "mid", "volume", "pearsons_correl"],
-n_steps=300, lookup_step=1, test_size=.2, n_layers=2, cell=LSTM, units=256, dropout=.4, 
-bidirectional=False, loss="huber_loss", optimizer="adam", batch_size=256, 
-epochs=800, patience=200, saveload=True, limit=4000)
-print("test 5 took " + str(time.time() - time_s))
+# time_s = time.time()
+# print("test 5")
+# the_real_test(test_year, test_month, test_day, test_days, 
+# ["open", "low", "high", "close", "mid", "volume", "pearsons_correl"],
+# n_steps=300, lookup_step=1, test_size=.2, n_layers=2, cell=LSTM, units=256, dropout=.4, 
+# bidirectional=False, loss="huber_loss", optimizer="adam", batch_size=256, 
+# epochs=800, patience=200, saveload=True, limit=4000)
+# print("test 5 took " + str(time.time() - time_s))
 
 # time_s = time.time()
 # print("test 6")
