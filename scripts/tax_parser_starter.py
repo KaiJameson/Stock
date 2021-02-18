88d4c43eb09236049a18c8a9a769ac3fa3b64d9f
kaij <kaijamesoncs@gmail.com>
2021-02-18 18:03:55 -0500

    a print statement for good measure

--- scripts/tax_parser_starter.py
+++ scripts/tax_parser_starter.py
@@ -3,6 +3,7 @@ import alpaca_trade_api as tradeapi
 import datetime 
 api = tradeapi.REST(real_api_key_id, real_api_secret_key, base_url="https://api.alpaca.markets")
 acts = api.get_activities()
+print("Dates printed as M/D/YYYY")
 for activity in acts:
     if (activity.activity_type == "FILL"):
       dt = activity.transaction_time.to_pydatetime()
